666974679197c71965b28732d97d7b3d31a54195
RamakrishnaVellala <itsp.kadapa@yahoo.com>
2023-02-12 21:30:43 +0530

    getting myprograms and all programs functionality added

--- G2P-Self-Service-Portal/controllers/main.py
+++ G2P-Self-Service-Portal/controllers/main.py
@@ -6,27 +6,53 @@ class Dashboard(Controller):
     @route("/allprograms", website=True, auth="public")
     def AllPrograms(self, **kw):
         programs = request.env["g2p.program"].sudo().search([]).sorted('id')
-        partner_id = request.env["res.users"].browse(request.session.uid).partner_id
-        memberships = request.env["g2p.program_membership"].sudo().search([('partner_id','=',partner_id.id)])
-        
+        partner_id = request.env.user.partner_id
+        states = {'draft': 'Submitted', 'enrolled': 'Enrolled'}
+
+        values = []
+        for program in programs:
+            membership = request.env["g2p.program_membership"].sudo().search(
+                [('partner_id', '=', partner_id.id), ('program_id', '=', program.id)])
+            values.append({
+                'id': program.id,
+                'name': program.name,
+                'has_applied': len(membership) > 0,
+                'status': states.get(membership.state, 'Error')
+
+            })
+
         return request.render(
             "G2P-Self-Service-Portal.allprograms",
             {
-                "programs": programs,
-                "partner_id":partner_id,
-                "memberships": memberships
-            },
+                'programs': values
+            }
         )
 
     @route("/", website=True, auth="public")
     def MyPrograms(self, **kw):
-        myprograms = request.env["g2p.program"].sudo().search([]).sorted('id')
-        partner_id = request.env["res.users"].browse(request.session.uid).partner_id
+
+        programs = request.env["g2p.program"].sudo().search([]).sorted('id')
+        partner_id = request.env.user.partner_id
+        states = {'draft': 'Submitted', 'enrolled': 'Enrolled'}
+
+        values = []
+        for program in programs:
+            membership = request.env["g2p.program_membership"].sudo().search(
+                [('partner_id', '=', partner_id.id), ('program_id', '=', program.id)])
+            ammount_issued = sum([ent.ammount for ent in request.env['g2p.entitlement'].sudo().search(
+                [('partner_id', '=', request.session.uid), ('program_id', '=', program.id)])])
+            values.append({
+                'id': program.id,
+                'name': program.name,
+                'has_applied': len(membership) > 0,
+                'status': states.get(membership.state, 'Error'),
+                'issued': ammount_issued,
+                'enrollment_date': membership.enrollment_date
+            })
 
         return request.render(
             "G2P-Self-Service-Portal.main_page",
             {
-                "programs": myprograms,
-
+                'programs': values
             },
         )
