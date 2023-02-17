3ed3430a5acffb28dbf64f7679f124d39f621f8e
mkumar-02 <mkumar6@ch.iitr.ac.in>
2023-02-17 09:50:45 +0530

    copied main repo

--- ssp_apply_for_program/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# from . import json_field
-from . import website_page
-from . import program_memebership
--- ssp_apply_for_program/models/program_memebership.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models
-
-class G2PProgramMembership(models.Model):
-    _inherit = "g2p.program_membership"
-
-    application_id = fields.Float("Application ID")
-
