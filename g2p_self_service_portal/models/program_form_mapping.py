47c5cb45d3d3f1537892284ecbf773487f5ee3b0
mkumar-02 <mkumar6@ch.iitr.ac.in>
2023-02-20 18:40:23 +0530

    form mapping filed

--- g2p_self_service_portal/models/program_form_mapping.py
+++ g2p_self_service_portal/models/program_form_mapping.py
@@ -3,4 +3,4 @@ from odoo import api, fields, models
 class G2PProgram(models.Model):
     _inherit = "g2p.program"
 
-    self_service_portal_form = fields.Many2one("website.page", "Program Form")
+    self_service_portal_form = fields.Selection([("form1", "Form 1")], "Program Form")
