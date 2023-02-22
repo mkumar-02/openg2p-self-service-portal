4e6e0679af197e506dd6942e22a033a26677b398
mkumar-02 <mkumar6@ch.iitr.ac.in>
2023-02-23 00:16:10 +0530

    Added toast message and basic validation in form

--- g2p_self_service_portal/static/src/js/self_service_form_action.js
+++ g2p_self_service_portal/static/src/js/self_service_form_action.js
@@ -8,8 +8,22 @@ function getQueryParams(url) {
   return params;
 }
 
+function showToast(message) {
+  const toastMessage = document.querySelector('#toast-message');
+  toastMessage.textContent = message;
+  const toastContainer = document.querySelector('#toast-container');
+  toastContainer.style.display = "block"
+}
+
+function hide_toast() {
+  const toastContainer = document.querySelector('#toast-container');
+  toastContainer.style.display = "none"
+}
+
+
 function form_submit_action(){
 
+  //URL Change
   var test = $('.s_website_form')
   var form = test.find('form')
   var url = form[0].baseURI
@@ -19,24 +33,33 @@ function form_submit_action(){
   form[0].action = '/selfservice'
   form[0].action = form[0].action.concat('/submitted?program_name='+ param)
 
-  count = 0;
-  var required_field= $('.s_website_form_required')
 
-  for(let i=0; i<required_field.length; i++){
-    var required_input_field= required_field.find($('.s_website_form_input'))[i]
+  // Validation's //
+  count = 0;
+  var required_fields = $('.s_website_form_required')
+
+  for(let i=0; i<required_fields.length; i++){
+    var required_input_field = required_fields.find($('.s_website_form_input'))[i]
 
     if(required_input_field.value == ''){
-      alert("Please update all mandantory fields");
-      required_input_field.style.borderColor= '#D32D2D'
-      required_input_field.reportValidity();
+      var field_name = "Please enter " + required_input_field.name.toLowerCase()
+      var error_message = '<div class="input-field-error-message">' + field_name + '</div>'
+      required_input_field.style.borderColor = '#D32D2D'
       count = 1;
-      break;
+      showToast('Please update all mandatory fields');
+
+      if(!required_fields.find($('.input-field-error-message'))[i]){
+        required_input_field.insertAdjacentHTML('afterend', error_message);
+      }
+    }
+    else{
+      required_input_field.style.borderColor = '#E3E3E3';
+      required_fields.find($('.input-field-error-message'))[i].style.display = "none"
     }
   }
-  if(count==0){
+
+  if(count == 0){
     form.submit()
   }
 
 }
-
-
