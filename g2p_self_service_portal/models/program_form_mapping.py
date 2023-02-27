e1ec3481095ec5cf9386a41b0a12d4730e458682
mkumar-02 <mkumar6@ch.iitr.ac.in>
2023-02-27 08:37:45 +0530

    added functonality to program form button and updated form validations

--- g2p_self_service_portal/models/program_form_mapping.py
+++ g2p_self_service_portal/models/program_form_mapping.py
@@ -1,25 +1,45 @@
-import xml.etree.ElementTree as ET
 from odoo import api, fields, models
 
 class G2PProgram(models.Model):
     _inherit = "g2p.program"
 
-    self_service_portal_form = fields.Many2one("website.page", "Program Form")
+    self_service_portal_form = fields.Many2one("website.page", "Program Form", domain=[('id', 'not in', [1, 2, 3])])
 
     def update_template(self):
         form_id= self.self_service_portal_form.id
-
         form_view_id= self.env["website.page"].sudo().search([("id", "=", form_id)]).view_id.id
-
         form_view_template= self.env["ir.ui.view"].sudo().search([("id", "=", form_view_id)]).arch_db
 
         index = form_view_template.find("</form>")
-        extra_text= '<input type="hidden" name="csrf_token" t-att-value="request.csrf_token()"/>'
+        csrf_generate_token_text= '<input type="hidden" name="csrf_token" t-att-value="request.csrf_token()"/>'
 
-        if extra_text not in form_view_template:
-            form_view_template = form_view_template[:index]+ extra_text+ form_view_template[index:]
+        if csrf_generate_token_text not in form_view_template:
+            form_view_template = form_view_template[:index] + csrf_generate_token_text + form_view_template[index:]
         else:
             pass
 
         self.env["ir.ui.view"].sudo().search([("id", "=", form_view_id)]).write({'arch_db':form_view_template.replace("website.layout", "g2p_self_service_portal.self_service_form_template")})
     
+        
+        tree_view = self.env.ref('g2p_programs.view_program_list_tree')
+        action = {
+            'type': 'ir.actions.act_window',
+            'view_mode': 'tree',
+            'res_model': 'g2p.program',
+            'views': [(tree_view.id, 'tree')]
+        }
+        return action
+    
+    def discard_template(self):
+        tree_view = self.env.ref('g2p_programs.view_program_list_tree')
+        action = {
+            'type': 'ir.actions.act_window',
+            'view_mode': 'tree',
+            'res_model': 'g2p.program',
+            'views': [(tree_view.id, 'tree')]
+        }
+        return action
+
+
+
+
--- g2p_self_service_portal/static/src/js/self_service_form_action.js
+++ g2p_self_service_portal/static/src/js/self_service_form_action.js
@@ -20,16 +20,21 @@ function hide_toast() {
   toast_container.style.display = "none"
 }
 
-function validate_email(email) {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailRegex.test(email);
+function isValidEmail(email) {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(email);
 }
 
-function validate_url(url) {
-  const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
-  return urlRegex.test(url);
+function isValidURL(url) {
+  const urlPattern = /^(https?:\/\/)?[a-z0-9-]+\.[a-z]{2,}(\.[a-z]{2,})?$/i;
+  return urlPattern.test(url);
 }
 
+function isValidTelNumber(input_str) {
+  var re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+
+  return re.test(input_str);
+}
 
 function form_submit_action(){
 
@@ -45,87 +50,134 @@ function form_submit_action(){
 
 
   // Validation's //
-  count = 0;
+  isValid = true;
+
   var required_fields = $('.s_website_form_required')
 
   for(let i=0; i<required_fields.length; i++){
-    
-    var required_input_field = required_fields.find($('.s_website_form_input'))[i]
+    var required_input_field = required_fields[i].getElementsByClassName('s_website_form_input')[0]
+    var field_name = required_input_field.name.toLowerCase()
+    var error_message = '<div class="input-field-error-message">Please enter ' + field_name + '</div>'
 
+    // null value
     if(required_input_field.value == ''){
-      var field_name = "Please enter " + required_input_field.name.toLowerCase()
-      var error_message = '<div class="input-field-error-message">' + field_name + '</div>'
+
       required_input_field.style.borderColor = '#D32D2D'
-      count = 1;
+      isValid = false;
       show_toast('Please update all mandatory fields');
 
-      if(!required_fields.find($('.input-field-error-message'))[i] == true){
-        if(required_input_field.classList.contains('datetimepicker-input')){
-          required_input_field.parentElement.insertAdjacentHTML('afterend', error_message);
+      if(required_input_field.type == 'radio' || required_input_field.type == 'checkbox'){
+
+      }
+
+      else{
+        if(required_fields[i].getElementsByClassName('input-field-error-message').length == 0){
+          required_fields[i].insertAdjacentHTML('beforeend', error_message);
         }
         else{
-          required_input_field.insertAdjacentHTML('afterend', error_message);
+          if(required_fields[i].getElementsByClassName('input-field-validation-message').length != 0){
+            required_fields[i].getElementsByClassName('input-field-validation-message')[0].style.display = 'none'
+          }
+          required_fields[i].getElementsByClassName('input-field-error-message')[0].style.display = 'block'
         }
-      }
-      else{
-        if(!required_fields.find($('.input-field-validation-message'))[i] == false){
-          required_fields.find($('.input-field-validation-message'))[i].style.display = 'none'
-        }
-        required_fields.find($('.input-field-error-message'))[i].style.display = 'block'
-        
       }
     }
-    else{
-      if(!required_fields.find($('.input-field-error-message'))[i] == false){
-        required_input_field.style.borderColor = '#E3E3E3';
-        required_fields.find($('.input-field-error-message'))[i].style.display = 'none'
-      }
-      if(required_input_field.type == 'email'){
-        if(validate_email(required_input_field.value) == false){
-          validation_message = '<div class="input-field-validation-message">Please enter a valid email address</div>'
-          count = 1;
-          required_input_field.style.borderColor = '#D32D2D';
-          show_toast('Please update all mandatory fields');
-          if(!required_fields.find($('.input-field-validation-message'))[i] == true){
-            required_input_field.insertAdjacentHTML('afterend', validation_message);
-          }
-        }
-      }
-      else if(required_input_field.type == 'radio'){
-        var radio_field_name = required_input_field.name;
-        if(form[0].elements[radio_field_name].value == ''){
-          field_name = "Please select " + required_input_field.name.toLowerCase()
-          error_message = '<div class="input-field-error-message">' + field_name + '</div>'
-          field_name = "Please enter " + required_input_field.name.toLowerCase()
-          count = 1;
-          required_input_field.style.borderColor = '#D32D2D';
-          show_toast('Please update all mandatory fields');
 
-          if(!required_fields.find($('.input-field-error-message'))[i] == true){
-            required_fields[i].insertAdjacentHTML('beforeend', error_message);
+    //checking valid value
+    else{
+      required_input_field.style.borderColor = '#E3E3E3';
+      //removing the error message of not filling the input field
+      if(required_fields[i].getElementsByClassName('input-field-error-message').length != 0){
+        required_fields[i].getElementsByClassName('input-field-error-message')[0].style.display = 'none'
+      }
+
+      if(required_fields[i].getElementsByClassName('input-field-validation-message').length != 0){
+        required_fields[i].getElementsByClassName('input-field-validation-message')[0].style.display = 'none'
+      }
+
+      if(required_input_field.type == 'email'){
+        
+        if(isValidEmail(required_input_field.value) == false){
+          isValid = false;
+          validation_message = '<div class="input-field-validation-message">Please enter a valid email address</div>'
+          required_input_field.style.borderColor = '#D32D2D';
+          show_toast('Please update all mandatory fields');
+    
+          if(required_fields[i].getElementsByClassName('input-field-validation-message').length == 0){
+            required_fields[i].insertAdjacentHTML('beforeend', validation_message);
           }
           else{
-            required_fields.find($('.input-field-error-message'))[i].style.display = 'block'
+            required_fields[i].getElementsByClassName('input-field-validation-message')[0].style.display = 'block'
           }
         }
       }
-
       else if(required_input_field.type == 'url'){
-        if(validate_url(required_input_field.value) == false){
+        
+        if(isValidURL(required_input_field.value) == false){
+          isValid = false;
           validation_message = '<div class="input-field-validation-message">Please enter a valid url</div>'
-          count = 1;
           show_toast('Please update all mandatory fields');
           required_input_field.style.borderColor = '#D32D2D';
-          if(!required_fields.find($('.input-field-validation-message'))[i] == true){
-            required_input_field.insertAdjacentHTML('afterend', validation_message);
+
+          if(required_fields[i].getElementsByClassName('input-field-validation-message').length == 0){
+            required_fields[i].insertAdjacentHTML('beforeend', validation_message);
           }
+          else{
+            required_fields[i].getElementsByClassName('input-field-validation-message')[0].style.display = 'block'
+          }
+        }
+      }
+      else if(required_input_field.type == 'tel'){
+        
+        if(isValidTelNumber(required_input_field.value) == false){
+          isValid = false;
+          validation_message = '<div class="input-field-validation-message">Please enter a valid telephone number</div>'
+          show_toast('Please update all mandatory fields');
+          required_input_field.style.borderColor = '#D32D2D';
+
+          if(required_fields[i].getElementsByClassName('input-field-validation-message').length == 0){
+            required_fields[i].insertAdjacentHTML('beforeend', validation_message);
+          }
+          else{
+            required_fields[i].getElementsByClassName('input-field-validation-message')[0].style.display = 'block'
+          }
+        }
+      }
+      else if(required_input_field.type == 'radio' || required_input_field.type == 'checkbox'){
+
+        var options = required_fields[i].getElementsByClassName('form-check-input')
+        var isChecked = false;
+
+        for(let j=0; j<options.length; j++){
+          // options[j].style.outline = 'none'
+
+          if(options[j].checked){
+            isChecked = true;
+          }
+        }
+
+        if(isChecked == false){
+          isValid = false;
+          var field_name = required_input_field.name.toLowerCase()
+          var select_error_message = '<div class="input-field-error-message">Please select ' + field_name + '</div>'
+
+          if(required_fields[i].getElementsByClassName('input-field-error-message').length == 0){
+            required_fields[i].insertAdjacentHTML('beforeend', select_error_message);
+          }
+          else{
+            required_fields[i].getElementsByClassName('input-field-error-message')[0].style.display = 'block'
+          }
+
+          // for(let j=0; j<options.length; j++){
+          //   options[j].style.outline = '1px solid #D32D2D'
+          // }          
         }
       }
     }
   }
 
-  if(count == 0){
-    form.submit()
+  if(isValid){
+    form.submit();
   }
 
 }
