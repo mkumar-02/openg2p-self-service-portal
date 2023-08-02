b6dad1ae34af5886701fbebc5eee4688734f2e5d
manoj-02 <mkumar6@ch.iitr.ac.in>
2023-08-02 17:16:10 +0530

    G2P-1044 Added condition check while mapping form with program.

--- g2p_service_provider_portal/models/programs.py
+++ g2p_service_provider_portal/models/programs.py
@@ -12,17 +12,18 @@ class G2PReimbursementProgram(models.Model):
     def update_form_template(self):
         if self.is_reimbursement_program:
             form_view = self.self_service_portal_form.view_id
-            form_view_template = form_view.arch_db
-            form_view.write(
-                {
-                    "arch_db": form_view_template.replace(
-                        "website.layout",
-                        "g2p_service_provider_portal.reimbursement_form_template_view",
-                    ).replace(
-                        "g2p_self_service_portal.self_service_form_template",
-                        "g2p_service_provider_portal.reimbursement_form_template_view",
-                    )
-                }
-            )
+            if form_view:
+                form_view_template = form_view.arch_db
+                form_view.write(
+                    {
+                        "arch_db": form_view_template.replace(
+                            "website.layout",
+                            "g2p_service_provider_portal.reimbursement_form_template_view",
+                        ).replace(
+                            "g2p_self_service_portal.self_service_form_template",
+                            "g2p_service_provider_portal.reimbursement_form_template_view",
+                        )
+                    }
+                )
         else:
             return super(G2PReimbursementProgram, self).update_form_template()
