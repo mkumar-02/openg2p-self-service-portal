92e310afccc1346f1ad46883a43ef112a6709571
Shivam <shivamg@iitkgp.ac.in>
2023-10-30 17:48:51 +0530

    resolved pre-commit issue

--- g2p_self_service_portal/static/src/js/self_service_signup_dob.js
+++ g2p_self_service_portal/static/src/js/self_service_signup_dob.js
@@ -1,12 +1,12 @@
+// eslint-disable-next-line no-unused-vars
 function validateDOB(selectedDate) {
     var today = new Date();
     var inputDate = new Date(selectedDate);
 
-    // Check if the selected date is in the future
     if (inputDate > today) {
         document.getElementById("dob-error").style.display = "block";
-        document.getElementById("birthdate").value = ""; // Clear the input
+        document.getElementById("birthdate").value = "";
     } else {
         document.getElementById("dob-error").style.display = "none";
     }
-    }
+}
